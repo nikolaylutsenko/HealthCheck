b5b86408aa3eac77cfb9a95b831d9e6f0a543ab9
Mykola Lutsenko <Mykola.Lutsenko@dataart.com>
2022-06-22 13:29:50 +0300

    (feat): Switch to SCSS, add new component WorldCities

---
+++ HealthCheck/src/app/world-cities/world-cities.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-world-cities',
+  templateUrl: './world-cities.component.html',
+  styleUrls: ['./world-cities.component.scss']
+})
+export class WorldCitiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- HealthCheckApi/Program.cs
+++ HealthCheckApi/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHealthChecks()
     .AddCheck("ICMP_01", new ICMPHealthCheck("www.ryadel.com", 100))
     .AddCheck("ICMP_02", new ICMPHealthCheck("www.google.com", 100))
-    .AddCheck("ICMP_03", new ICMPHealthCheck($"www.{Guid.NewGuid:N}.com", 100));
+    .AddCheck("ICMP_03", new ICMPHealthCheck($"10.0.0.0", 100));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
