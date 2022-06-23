fbfea43109d7d2670103cddb9f27f2039672ece6
Mykola Lutsenko <Mykola.Lutsenko@dataart.com>
2022-06-23 13:39:03 +0300

    (feat): Add dbcontext, add migration, add xlsx for source to seed data, add controllers for Countries and Cities

---
+++ HealthCheckApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using HealthCheckApi.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthCheckApi.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext() : base()
+    {
+    }
+    public ApplicationDbContext(DbContextOptions options)
+    : base(options)
+    {
+    }
+    public DbSet<City> Cities => Set<City>();
+    public DbSet<Country> Countries => Set<Country>();
+}
--- HealthCheckApi/Program.cs
+++ HealthCheckApi/Program.cs
@@ -1,4 +1,6 @@
+using HealthCheckApi.Data;
 using HealthCheckAPI;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add ApplicationDbContext and SQL Server support
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
