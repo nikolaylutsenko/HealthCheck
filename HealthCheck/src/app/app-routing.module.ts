69d109a81c04c5770bd06ee87165bb3995e88f0f
Mykola Lutsenko <Mykola.Lutsenko@dataart.com>
2022-07-07 16:02:06 +0300

    (feat): Add city edit component

--- HealthCheck/src/app/app-routing.module.ts
+++ HealthCheck/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { HealthCheckComponent } from './health-check/health-check.component';
 import { WorldCitiesComponent } from './world-cities/world-cities.component';
 import { CitiesComponent } from './cities/cities.component';
+import { CityEditComponent } from './cities/city-edit.component';
 import { CountriesComponent } from './countries/countries.component';
 
 const routes: Routes = [
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'health-check', component: HealthCheckComponent },
   { path: 'world-cities', component: WorldCitiesComponent },
   { path: 'cities', component: CitiesComponent },
+  { path: 'city/:id', component: CityEditComponent },
   { path: 'countries', component: CountriesComponent },
 ];
 
--- HealthCheck/src/app/app.module.ts
+++ HealthCheck/src/app/app.module.ts
@@ -15,7 +15,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WorldCitiesComponent } from './world-cities/world-cities.component';
 import { CitiesComponent } from './cities/cities.component';
 import { AngularMaterialModule } from './angular-material.module';
-import { CountriesComponent } from './countries/countries.component'
+import { CountriesComponent } from './countries/countries.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CityEditComponent } from './cities/city-edit.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,8 @@ import { CountriesComponent } from './countries/countries.component'
     HealthCheckComponent,
     WorldCitiesComponent,
     CitiesComponent,
-    CountriesComponent
+    CountriesComponent,
+    CityEditComponent
   ],
   imports: [
     BrowserModule,
@@ -36,7 +39,8 @@ import { CountriesComponent } from './countries/countries.component'
     MatButtonModule,
     MatIconModule,
     MatToolbarModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
